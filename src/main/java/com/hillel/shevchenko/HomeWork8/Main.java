2b909db4d42f8342b7770fb493a0bcc8690de11d
Shevchenko Anton <anton.shevchenko999777@gmail.com>
2022-12-10 02:30:44 +0100

    add HomeWork8

---
+++ src/main/java/com/hillel/shevchenko/HomeWork8/Main.java
@@ -0,0 +1,26 @@
+package com.hillel.shevchenko.HomeWork8;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int badnumb1 = 4;
+        int badnumb2 = 9;
+        int counter = 0;
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i / 10 == badnumb1 || i % 10 == badnumb1) {
+                continue;
+            } else if (i / 10 == badnumb2 || i % 10 == badnumb2) {
+                continue;
+            }
+            System.out.println("number shuttle " + " = " + i);
+            counter++;
+        }
+
+        System.out.println("counter shuttle = " + counter);
+    }
+    }
+
+
+
