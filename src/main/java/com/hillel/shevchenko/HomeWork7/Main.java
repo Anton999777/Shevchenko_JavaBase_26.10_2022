069bb23ba4f61a93aacafe37daeee1ffa031e09b
Shevchenko Anton <anton.shevchenko999777@gmail.com>
2022-12-10 02:32:11 +0100

    add HomeWork7

---
+++ src/main/java/com/hillel/shevchenko/HomeWork7/Main.java
@@ -0,0 +1,44 @@
+package com.hillel.shevchenko.HomeWork7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int random = (int) (Math.random() * 11);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberUser = 0;
+        System.out.println("Please guess the number from 0 to 10");
+
+        int numberOfAttempts = 4;
+
+        while (numberOfAttempts > 0) {
+            if (scanner.hasNextInt()) {
+
+                numberUser = scanner.nextInt();
+                System.out.println(numberUser);
+
+                if (random < numberUser) {
+                    System.out.println("Try again, your number is more than mystical");
+                } else if (random > numberUser) {
+                    System.out.println("Try again, your number is less than mystical");
+                } else {
+                    System.out.println("YOU WIN!!!");
+                    break;
+                } numberOfAttempts--;
+
+                if (numberOfAttempts == 0) {
+                    System.out.println("You lost :( computer number " + random + " and your number " + numberUser);
+                    break;
+                }
+            } else {
+                System.out.println("Wrong data! Try again");
+                scanner.nextLine();
+            }
+            }
+
+        }
+}
+
