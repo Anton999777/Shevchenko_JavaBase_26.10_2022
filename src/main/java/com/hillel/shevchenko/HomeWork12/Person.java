4305b80269bc4a96c7707cf222456521dd377639
Shevchenko Anton <anton.shevchenko999777@gmail.com>
2022-12-23 18:32:53 +0100

    add HomeWork11

---
+++ src/main/java/com/hillel/shevchenko/HomeWork12/Person.java
@@ -0,0 +1,20 @@
+package com.hillel.shevchenko.HomeWork12;
+
+public class Person {
+    public static void main(String[] args) {
+       personInfo("Anton", "Shevchenko", "Kharkiv", "43534534543");
+        personInfo("Will", "Smith", "New York", "444444444");
+        personInfo("Jackie", "Chan", "Shanghai", "4565463435345");
+
+
+    }
+
+    static String personInfo (String name, String surname, String city, String phoneNumber) {
+        String a = "Зателефонувати громадянинові " + name + " " + surname + " із міста " + city +
+                " можна за номером " + phoneNumber;
+        return a;
+    }
+//        System.out.println("Зателефонувати громадянинові " + name + " " + surname + " із міста " + city +
+//                " можна за номером " + phoneNumber)
+
+}
